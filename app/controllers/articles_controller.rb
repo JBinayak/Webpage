76243020c694d3d9da70a05d3b26572015037b8f
JBinayak <binayakgurubacharya@gmail.com>
2019-04-29 17:12:34 +0545

    adding show action (incomplete)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,12 @@
     def create
         #render plain: params[:article].inspect
         @article= Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        if @article.save
+            flash[:notice]= "Article was successfully created" 
+        redirect_to articles_path(@article)
+        else 
+            render 'new'
+        end
     end
 
     private
