cc7f5e22aa7c58b0e5833537ff3afa123bc21317
JBinayak <binayakgurubacharya@gmail.com>
2019-04-29 16:16:17 +0545

    edits on the article page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-    validates :title, presence: true , minimun: 3, maximum: 50
-    validates :description, presence: true , minimun: 10, maximum: 100
+    validates :title, presence: true , length: {minimun: 3, maximum: 50}
+    validates :description, presence: true , length:  {minimun: 10, maximum: 100}
 end
