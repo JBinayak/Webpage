cb90405fc39140c2fbbc7f7013e95f51163db472
JBinayak <binayakgurubacharya@gmail.com>
2019-04-30 11:20:37 +0545

    Add edit, show, update and links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@
         @article= Article.new
     end
  
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         #render plain: params[:article].inspect
         @article= Article.new(article_params)
@@ -11,6 +15,16 @@
             redirect_to article_path(@article)
         else 
             render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
         end
     end
 
